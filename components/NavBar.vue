c32f10d56c1a2bee1a85f7c19f3451c9dee2ad51
Bernhard <berni.nops.kofler@gmail.com>
2023-12-09 16:52:23 +0100

    added navbar.vue - added all pages for navbar - changed .gitignore

---
+++ components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul class="flex flex-row">
+      <li class="p-3"><NuxtLink to="/">Home</NuxtLink></li>
+      <li class="p-3"><NuxtLink to="gpt">Gpt</NuxtLink></li>
+      <li class="p-3"><NuxtLink to="clock">Clock</NuxtLink></li>
+      <li class="p-3"><NuxtLink to="chat">Chat</NuxtLink></li>
+      <li class="p-3"><NuxtLink to="music">Music</NuxtLink></li>
+      <li class="p-3"><NuxtLink to="weather">Weather</NuxtLink></li>
+      <li class="p-3"><NuxtLink to="notes">Notes</NuxtLink></li>
+      <li class="p-3"><NuxtLink to="calendar">Calendar</NuxtLink></li>
+      <li class="p-3"><NuxtLink to="youtube">Youtube</NuxtLink></li>
+      <li class="p-3"><NuxtLink to="settings">Settings</NuxtLink></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/weather.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Weather</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
