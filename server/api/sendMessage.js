d5efbe875e66b9b43b1ad405c0fdeea6a4c477eb
bernhard <berni.nops.kofler@gmail.com>
2024-01-08 19:18:27 +0100

    Add TypeScript support and update dependencies

---
+++ server/api/sendMessage.js
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (event) => {
+  return {
+    //this should return a message from the chatgpt api and send it to the ui layer to be displayed
+    //this should also save the message to local storage
+     
+  }
+})
